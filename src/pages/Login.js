3d4f8f9fe083a62cdfca547d172514fcc6b190eb
Moyosore <msontop.me@gmail.com>
2023-08-20 02:01:16 +0100

    starting

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <>
+            <div className='h-[100vh] flex items-center justify-center'>
+                <div className='w-[60%] h-[70%] bg-[white] rounded-xl'>
+                    <p>Login</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
